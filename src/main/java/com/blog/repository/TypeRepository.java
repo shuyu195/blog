d645a711a42dc3a97b00e902d00a65fa2394e9f1
shuyu195 <arya3378@126.com>
2020-12-12 21:25:42 +0800

    添加了 分类功能，实现增删查改，页面切换 in dev

---
+++ src/main/java/com/blog/repository/TypeRepository.java
@@ -0,0 +1,8 @@
+package com.blog.repository;
+
+import com.blog.po.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type,Long> {
+    Type findByName(String name);
+}
