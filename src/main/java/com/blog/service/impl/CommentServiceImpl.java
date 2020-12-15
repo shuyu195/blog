32ba5100b76d486c67f9108f9d640819bd810bca
shuyu195 <arya3378@126.com>
2020-12-15 12:58:23 +0800

    完成了 评论信息提交与回复功能 in dev

---
+++ src/main/java/com/blog/service/impl/CommentServiceImpl.java
@@ -0,0 +1,38 @@
+package com.blog.service.impl;
+
+import com.blog.po.Comment;
+import com.blog.repository.CommentRepository;
+import com.blog.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Override
+    public List<Comment> listCommentByBlogId(Long blogId) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "createTime");
+        return commentRepository.findByBlogId(blogId, sort);
+    }
+
+    @Transactional
+    @Override
+    public Comment saveComment(Comment comment) {
+        Long parentCommentId = comment.getParentComment().getId();
+        if (parentCommentId != -1) {
+            comment.setParentComment(commentRepository.findById(parentCommentId).get());
+        } else {
+            comment.setParentComment(null);
+        }
+        comment.setCreateTime(new Date());
+        return commentRepository.save(comment);
+    }
+}
