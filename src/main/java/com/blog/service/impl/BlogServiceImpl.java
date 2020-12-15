a77eaa5db136f51858196f50b87bf547029ff1cc
shuyu195 <arya3378@126.com>
2020-12-15 20:51:04 +0800

    完成了 归档功能 in dev

--- src/main/java/com/blog/service/impl/BlogServiceImpl.java
+++ src/main/java/com/blog/service/impl/BlogServiceImpl.java
@@ -19,9 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -114,6 +112,21 @@ public class BlogServiceImpl implements BlogService {
         Sort sort = Sort.by(Sort.Direction.DESC, "updateTime");
         Pageable pageable = PageRequest.of(0, size, sort);
         return blogRepository.findTop(pageable);
+    }
+
+    @Override
+    public Long countBlog() {
+        return blogRepository.count();
+    }
+
+    @Override
+    public Map<String, List<Blog>> archiveBlog() {
+        List<String> years = blogRepository.findGroupYear();
+        Map<String, List<Blog>> map = new LinkedHashMap<>();
+        for (String year : years) {
+            map.put(year, blogRepository.findByYear(year));
+        }
+        return map;
     }
 
     @Transactional
