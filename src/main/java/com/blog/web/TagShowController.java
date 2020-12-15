1c142ce58bc15b09fc682017e0abbd3495900a09
shuyu195 <arya3378@126.com>
2020-12-15 17:07:42 +0800

    完成了 标签页面展示 in dev

---
+++ src/main/java/com/blog/web/TagShowController.java
@@ -0,0 +1,41 @@
+package com.blog.web;
+
+import com.blog.po.Tag;
+import com.blog.po.Type;
+import com.blog.service.BlogService;
+import com.blog.service.TagService;
+import com.blog.service.TypeService;
+import com.blog.vo.BlogQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class TagShowController {
+
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private BlogService blogService;
+
+    @GetMapping("/tags/{id}")
+    public String types(@PathVariable Long id, @PageableDefault(size = 10, sort = "updateTime", direction = Sort.Direction.DESC) Pageable pageable,
+                        Model model) {
+        List<Tag> tags = tagService.listTagTop(1000);
+        if (id == -1) {
+            id = tags.get(0).getId();
+        }
+        model.addAttribute("tags", tags);
+        model.addAttribute("page", blogService.listBlog(id, pageable));
+        model.addAttribute("activeTagId", id);
+        return "tags";
+    }
+}
